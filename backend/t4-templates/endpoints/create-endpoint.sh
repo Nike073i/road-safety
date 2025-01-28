9781331f08a0ce285a95cded2b8dc5745d5bc356
skuld <nike073i@mail.ru>
2025-01-28 21:01:46 +0400

    add: t4 шаблоны

---
+++ backend/t4-templates/endpoints/create-endpoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+OUTPUT_DIR=`pwd`
+OUTPUT_DIR_NAME=`basename $OUTPUT_DIR`
+
+if [ -z "$1" ]; then
+  NAME=$OUTPUT_DIR_NAME
+else
+  NAME=$1
+fi
+
+SCRIPT_DIR=`dirname $0`
+
+TEMPLATE_NAME=endpoint.tt
+
+TEMPLATE_PATH=$SCRIPT_DIR/$TEMPLATE_NAME
+
+dotnet t4 -p:Name="$NAME" -o $OUTPUT_DIR/$NAME "$TEMPLATE_PATH"
